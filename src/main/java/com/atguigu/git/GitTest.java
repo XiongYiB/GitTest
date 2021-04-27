142b0ed6c97e621a8345804b7e34d7c0c15c2125
Xyb <XiongYiB0612@163.com>
2021-04-27 21:09:10 +0800

    hot-fix Commit1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("Hello Git");
         System.out.println("Hello Git!!!");
         System.out.println("Hello Git  third");
+        System.out.println("Hello Fix!");
     }
 }
